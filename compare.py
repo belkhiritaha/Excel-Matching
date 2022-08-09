d3e76ebdf0310ddc9467b504bf13a61bd6af8686
belkhiritaha <belkhiri.taha53@gmail.com>
2022-08-09 12:21:23 +0100

    the "correcting" script not behaving how it's supposed to

--- compare.py
+++ compare.py
@@ -1,3 +1,4 @@
+import numpy
 import pandas as pd
 
 ficheDF = pd.read_excel("output.xlsx")
@@ -20,6 +21,12 @@ print(systemDF)
 # index 3: Quantities
 # index 4: Tax_Values
 
+SH_Codes = 0
+DeclaredValues = 1
+Tax_Codes = 2
+Quantities = 3
+Tax_Values = 4
+
 matches = []
 
 # loop through each line in ficheDF and check if line is in systemDF
@@ -27,19 +34,19 @@ for index, row in ficheDF.iterrows():
     # check if SH_Code is in systemDF
     if row[0] in systemDF['SH_Codes']:
         # get list of indexes of row in systemDF
-        indexList = systemDF[systemDF['SH_Codes'] == row[0]].index
+        indexList = systemDF[systemDF['SH_Codes'] == row[SH_Codes]].index
         # loop through each index in indexList
         for i in indexList:
             # check if DeclaredValue is equal to systemDeclaredValue
-            if row[1] == systemDF['DeclaredValues'][i]:
+            if row[DeclaredValues] == systemDF['DeclaredValues'][i]:
                 # check if Tax_Code is equal to systemTax_Code
-                if row[2] == systemDF['Tax_Codes'][i]:
+                if row[Tax_Codes] == systemDF['Tax_Codes'][i]:
                     # check if Quantity is equal to systemQuantity
-                    if row[3] == systemDF['Quantities'][i]:
+                    if row[Quantities] == systemDF['Quantities'][i]:
                         # check if Tax_Value is equal to systemTax_Value
-                        if row[4] == systemDF['Tax_Values'][i]:
-                            #print("Found perfect match: ")
-                            #print(index, i)
+                        if row[Tax_Values] == systemDF['Tax_Values'][i]:
+                            print("Found perfect match: ")
+                            print(index, i)
                             # save i and index into matches list
                             matches.append([i, index])
                             break
@@ -47,9 +54,11 @@ for index, row in ficheDF.iterrows():
 print(matches)
 # remove matches from ficheDF and systemDF
 for i in matches:
-    ficheDF = ficheDF.drop(i[0])
+    ficheDF = ficheDF.drop(i[1])
     systemDF = systemDF.drop(i[0])
 
+ficheDF.reset_index(drop=True, inplace=True)
+systemDF.reset_index(drop=True, inplace=True)
 print(ficheDF)
 print(systemDF)
 
@@ -57,28 +66,35 @@ corrected = []
 # correct errors in DeclaredValues and Tax_Values and Quantities
 for index, row in ficheDF.iterrows():
     # check if SH_Code is in systemDF
-    if row[0] in systemDF['SH_Codes']:
+
+    if row[SH_Codes] in systemDF['SH_Codes']:
         # get list of indexes of row in systemDF
-        indexList = systemDF[systemDF['SH_Codes'] == row[0]].index
+        indexList = systemDF[systemDF['SH_Codes'] == row[SH_Codes]].index
+        print("IndexList: " + indexList)
         # loop through each index in indexList
         for i in indexList:
+            print("lol")
             # check if Tax_Code is equal to systemTax_Code
-            if row[2] == systemDF['Tax_Codes'][i]:
+            if row[Tax_Codes] == systemDF['Tax_Codes'][i]:
                 # replace systemDeclaredValue with ficheDeclaredValue
-                systemDF['DeclaredValues'][i] = row[1]
+                systemDF['DeclaredValues'][i] = row[DeclaredValues]
                 # replace systemTax_Value with ficheTax_Value
-                systemDF['Tax_Values'][i] = row[4]
+                systemDF['Tax_Values'][i] = row[Tax_Values]
                 # replace systemQuantity with ficheQuantity
-                systemDF['Quantities'][i] = row[3]
+                systemDF['Quantities'][i] = row[Quantities]
                 # save i and index into corrected list
+                print("Corrected: " + str(index) + " " + str(i))
                 corrected.append([i, index])
                 break
 
+print("Corrected: ", corrected)
 # remove corrected from ficheDF and systemDF
 for i in corrected:
-    ficheDF = ficheDF.drop(i[0])
-    systemDF = systemDF.drop(i[0])
+    ficheDF = ficheDF.drop(i[0], inplace=True)
+    systemDF = systemDF.drop(i[0], inplace=True)
 
+ficheDF.reset_index(drop=True, inplace=True)
+systemDF.reset_index(drop=True, inplace=True)
 # the algorithm:
 
 # count number of lines left, each line is a bit for a binary number, loop through each number in binary and check if sumQuantity is equal to systemQuantity
@@ -87,18 +103,21 @@ for i in corrected:
 # if sumQuantity is not equal to systemQuantity, go to next binary
 
 # count number of lines left
-linesLeft = ficheDF.shape[0]
-print(linesLeft)
+linesLeftFiche = ficheDF.shape[0]
+linesLeftSys = systemDF.shape[0]
+groups = []
 
 # loop through each lineLeft
-for i in range(linesLeft):
+for i in range(linesLeftSys):
     # loop through each binary number
-    for j in range(2**linesLeft):
+    for j in range(1, 2**linesLeftFiche):
         # get binary representation of j
         binary = bin(j)[2:]
+        while len(binary) < linesLeftFiche:
+            binary = '0' + binary
         # get sumQuantity
         sumQuantity = 0
-        for k in range(linesLeft):
+        for k in range(linesLeftFiche):
             if binary[k] == '1':
                 # add quantity to sumQuantity
                 sumQuantity += ficheDF['Quantities'][k]
@@ -106,14 +125,19 @@ for i in range(linesLeft):
         if sumQuantity == systemDF['Quantities'][i]:
             # get sumDeclaredValue
             sumDeclaredValue = 0
-            for k in range(linesLeft):
+            for k in range(linesLeftFiche):
                 if binary[k] == '1':
                     # add quantity to sumDeclaredValue
                     sumDeclaredValue += ficheDF['DeclaredValues'][k]
             # check if sumDeclaredValue is close enough to systemDeclaredValue
-            if abs(sumDeclaredValue - systemDF['DeclaredValues'][i]) < 0.01:
+            if abs(sumDeclaredValue - systemDF['DeclaredValues'][i]) < 1.5:
                 # get current binary number
-                currentBinary = j
+                currentBinary = '0b' + binary
                 # add currentBinary to matches list
-                matches.append([currentBinary, i])
+                groups.append([currentBinary, i])
                 break
+
+print(groups)
+
+print(ficheDF)
+print(systemDF)
